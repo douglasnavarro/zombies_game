5973c7a27f29acc256c5130200fe41374ec972c0
Douglas Navarro <douglasnavarro94@gmail.com>
2020-02-15 12:21:06 -0300

    define and draw zombie

--- game.js
+++ game.js
@@ -1,16 +1,23 @@
 let survivor;
+let zombie;
 
 function drawSurvivor(survivor) {
   line(0, 0, survivor.x, survivor.y);
 }
 
+function drawZombie(zombie) {
+  circle(zombie.x, zombie.y, 35);
+}
+
 function setup() {
   createCanvas(400, 400);
   survivor = p5.Vector.fromAngle(radians(45), 50);
+  zombie = p5.Vector.fromAngle(radians(-90), 75);
 }
 
 function draw() {
   background(220);
   translate(width/2, height/2);
   drawSurvivor(survivor);
+  drawZombie(zombie);
 }
