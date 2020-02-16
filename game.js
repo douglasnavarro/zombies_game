67b8532e2c530c0beb406ba524330198d7c7a2ee
Douglas Navarro <douglasnavarro94@gmail.com>
2020-02-16 11:44:50 -0300

    refactor creation of zombie and survivor

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-let survivor;
-let zombie;
+let gameSurvivor;
+let gameZombie;
 
 function drawSurvivor(survivor) {
   line(0, 0, survivor.x, survivor.y);
@@ -9,22 +9,33 @@ function drawZombie(zombie) {
   circle(zombie.x, zombie.y, 35);
 }
 
+// Pure functions
+
+function createSurvivor() {
+  return p5.Vector.fromAngle(radians(45), 50);
+}
+
+function createZombie(distance = 75) {
+  return p5.Vector.fromAngle(radians(-90), distance);
+}
+
 function moveZombie(zombie) {
   return p5.Vector.fromAngle(zombie.heading(), zombie.mag() - 1);
 }
 
+// p5Js functions
 
 function setup() {
   createCanvas(400, 400);
-  survivor = p5.Vector.fromAngle(radians(45), 50);
-  zombie = p5.Vector.fromAngle(radians(-90), 75);
+  gameSurvivor = createSurvivor();
+  gameZombie = createZombie();
 }
 
 function draw() {
-  zombie = moveZombie(zombie);
-  
+  gameZombie = moveZombie(gameZombie);
+
   background(220);
-  translate(width/2, height/2);
-  drawSurvivor(survivor);
-  drawZombie(zombie);
+  translate(width / 2, height / 2);
+  drawSurvivor(gameSurvivor);
+  drawZombie(gameZombie);
 }
